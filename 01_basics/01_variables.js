d520721d9eaacd5a5c476c4552d231cb79609a09
patilprasad21 <122592869+patilprasad21@users.noreply.github.com>
2025-01-15 15:09:08 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+// var name="prasad";
+// name="rahul";
+// console.log(name);
+// {
+//     var name="sam";
+//     console.log("Inside block : ",name);
+// }
+// console.log("outside block : ",name);
+
+// Employees Details
+
+let companyName="infosys pvt limited";
+const employeeName="prasad sanjay patil";
+let employeeSalary=123456+"/month";
+const companyAddress="opp bajaj road,pune 415-502";
+var companyEnvironment="green structure";
+console.table([companyName,employeeName,employeeSalary,companyAddress,companyEnvironment]);
+
+/*
+prefer let instead of var ,
+beacause issues in block and functionl scopes
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"  // treats as  newwe versions 
+
+// datatypes details
+//number,bigint,string,boolean,null,undefined,symbol, object
+
+let num=20;//number datatype
+let str="prasad";//sting datatype
+let isLogged=false;//boolean datatype
+
+let customerName = "prasad";
+let _ID=12345;
+let _isAvailable=false;
+let _isDissable=undefined;
+let _isEmplty=null;
+
+console.table([customerName,_ID,_isAvailable,_isDissable,_isEmplty])
+let types=typeof undefined; // type of undefined is "undefined"
+let types2=typeof null;// type of null is "object"
+console.table([types,types2]);
+
+
+
+
+
