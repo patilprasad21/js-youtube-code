291f86bf88c357da5a1838d2f78588abf1103d97
patilprasad21 <122592869+patilprasad21@users.noreply.github.com>
2025-01-15 15:38:12 +0000

    operationConvert

---
+++ 01_basics/03_conversionOperators.js
@@ -0,0 +1,32 @@
+let gravity=9.81;
+console.log(typeof gravity);
+//OR
+console.log(typeof(gravity));
+
+//convert numer to string
+let newGravity=String(gravity);
+console.log(newGravity)
+console.log(typeof(newGravity))
+//convert string to boolean
+let GG=Boolean(newGravity);
+console.log(GG);
+
+// "33" => 33
+//"333asd" => NaN
+//true => 1;false => 0
+
+let isLoggin="";
+
+let booleanIsloggin=Boolean(isLoggin);
+console.log(booleanIsloggin);
+
+//1 => true; 0 => false
+//"" => false
+//"prasad" => true
+
+console.log(typeof NaN);
+
+
+
+
+
