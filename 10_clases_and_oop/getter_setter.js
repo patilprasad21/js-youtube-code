a8d7f5fcfb2e8df6087616a74411f310e4b30d02
patilprasad21 <122592869+patilprasad21@users.noreply.github.com>
2025-02-13 02:30:38 +0000

    getters and setters

---
+++ 10_clases_and_oop/getter_setter.js
@@ -0,0 +1,27 @@
+class User {
+    constructor(email, password){
+        this.email = email,
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+    }
+
+    set email(value){
+     this._email = value;
+    }
+
+    get password(){
+        return `${this._password}random`
+    }
+
+    set password(value){
+        this._password = value;
+    }
+}
+
+const prasad = new User("pprahul@google.com", "abc")
+console.log(prasad.password);
+console.log(prasad.email);
+
---
+++ 10_clases_and_oop/mathpi.js
@@ -0,0 +1,35 @@
+const descriptor = Object.getOwnPropertyDescriptor(Math, "PI");
+
+console.log(descriptor);
+
+// console.log(Math.PI);
+// Math.PI = 4;
+// console.log(Math.PI);
+
+
+const TV = {
+   name : "samsung",
+   price: 123456,
+   isAvailable : true,
+
+   orderTV: function(){
+     log("TV OntheWay");
+   }
+
+}
+
+// console.log(Object.getOwnPropertyDescriptor(TV, "name"));
+
+
+Object.defineProperty(TV, "name", {
+    // writable: false,
+    enumerable: false
+})
+
+// console.log(Object.getOwnPropertyDescriptor(TV, "name"));
+
+
+for (let [key, value] of Object.entries(TV)) {
+    if(typeof value !== 'function')
+    console.log(`${key} : ${value}`)
+}
